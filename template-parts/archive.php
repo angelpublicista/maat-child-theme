458e7008d2556022e257c6527f9dc9c84f89738f
Angeldevelop <geniorama.info@gmail.com>
2022-09-14 21:36:14 -0500

    feat: avance blog datos dinamicos

--- template-parts/archive.php
+++ template-parts/archive.php
@@ -14,14 +14,6 @@ if ( ! defined( 'ABSPATH' ) ) {
     ?>
 
     <?php echo do_shortcode('[maat_heading_blog title='.($archive_title).']'); ?>
-	<?php if ( apply_filters( 'hello_elementor_page_title', true ) ) : ?>
-		<header class="page-header">
-			<?php
-			the_archive_title( '<h1 class="entry-title">', '</h1>' );
-			the_archive_description( '<p class="archive-description">', '</p>' );
-			?>
-		</header>
-	<?php endif; ?>
 
     <div class="maat-container">
         <div class="page-content">
@@ -31,7 +23,14 @@ if ( ! defined( 'ABSPATH' ) ) {
             </div>
             <div class="maat-grid-blog-filter">
                 <div class="maat-grid-blog-filter__view">
-                    <span>Mostrando <b>6</b> de <b>100</b></span>
+                <?php
+                    global $wp_query;
+                    $pagenum = $wp_query->query_vars['paged'] < 1 ? 1 : $wp_query->query_vars['paged'];
+                    $first = ( ( $pagenum - 1 ) * $wp_query->query_vars['posts_per_page'] ) + 1;
+                    $last = $first + $wp_query->post_count - 1;
+                    $text_number_posts = "Mostrando <b> $first - $last </b> de <b>$wp_query->found_posts</b>";
+                ?>
+                    <span><?php echo $text_number_posts ?></span>
                 </div>
                 <div class="maat-grid-blog-filter__filter">
                     <span>Ordenar por</span>
@@ -46,94 +45,82 @@ if ( ! defined( 'ABSPATH' ) ) {
                 <hr class="maat-grid-blog__divider">
             </div>
             <div class="maat-grid-blog-posts">
-                <div class="maat-grid-blog-posts__row">
-                    <div class="maat-grid-blog-posts__col">
-                        <img class="maat-grid-blog-posts__col__img" src="https://images.pexels.com/photos/13421007/pexels-photo-13421007.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="">
-                    </div>
-                    <div class="maat-grid-blog-posts__col">
-                        <span class="maat-grid-blog-posts__col__etiqueta etiqueta-verde">Gestión de residuos</span>
-                        <h2 class="maat-grid-blog-posts__col__title">Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit.</h2>
-                        <div class="maat-grid-blog-posts__col__info">
-                            <span class="maat-grid-blog-posts__col__info__date">
-                                <i class="fa-solid fa-calendar-days"></i>
-                                <p>23 agosto, 2022</p>
-                            </span>
-                            <span class="maat-grid-blog-posts__col__info__author">
-                                <i class="fa-regular fa-user"></i>
-                                <p>Por Erick Jiménez</p>
-                            </span>
-                        </div>
-                        <hr class="maat-grid-blog-posts__col__divider">
-                        <p class="maat-grid-blog-posts__col__desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et dapibus habitant elementum, faucibus luctus quam aliquet justo. Arcu nascetur non in gravida eu porttitor. Tincidunt sed sed sapien integer. Et pulvinar nunc ullamcorper sagittis,</p>
-                        <a href="#" class="maat-grid-blog-posts__col__link">
-                            LEER MÁS
-                        </a>
-                    </div>
-                </div>
+                <?php
+                while ( have_posts() ) {
+                    the_post();
+                    $post_link = get_permalink();
+                    $post_date = get_the_date();
+                    $post_author_fn = get_the_author_firstname();
+                    $post_author_ln = get_the_author_lastname();
+                    $post_author_full = $post_author_fn . " " . $post_author_ln;
+                    $post_cats = get_the_category();
 
-                <div class="maat-grid-blog-posts__row">
-                    <div class="maat-grid-blog-posts__col">
-                        <img class="maat-grid-blog-posts__col__img" src="https://images.pexels.com/photos/13421007/pexels-photo-13421007.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="">
-                    </div>
-                    <div class="maat-grid-blog-posts__col">
-                        <span class="maat-grid-blog-posts__col__etiqueta etiqueta-azul">Gestión de residuos</span>
-                        <h2 class="maat-grid-blog-posts__col__title">Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit.</h2>
-                        <div class="maat-grid-blog-posts__col__info">
-                            <span class="maat-grid-blog-posts__col__info__date">
-                                <i class="fa-solid fa-calendar-days"></i>
-                                <p>23 agosto, 2022</p>
-                            </span>
-                            <span class="maat-grid-blog-posts__col__info__author">
-                                <i class="fa-regular fa-user"></i>
-                                <p>Por Erick Jiménez</p>
-                            </span>
-                        </div>
-                        <hr class="maat-grid-blog-posts__col__divider">
-                        <p class="maat-grid-blog-posts__col__desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et dapibus habitant elementum, faucibus luctus quam aliquet justo. Arcu nascetur non in gravida eu porttitor. Tincidunt sed sed sapien integer. Et pulvinar nunc ullamcorper sagittis,</p>
-                        <a href="#" class="maat-grid-blog-posts__col__link">
-                            LEER MÁS
-                        </a>
-                    </div>
-                </div>
-
-                <div class="maat-grid-blog-posts__row">
-                    <div class="maat-grid-blog-posts__col">
-                        <img class="maat-grid-blog-posts__col__img" src="https://images.pexels.com/photos/13421007/pexels-photo-13421007.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="">
-                    </div>
-                    <div class="maat-grid-blog-posts__col">
-                        <span class="maat-grid-blog-posts__col__etiqueta etiqueta-roja">Gestión de residuos</span>
-                        <h2 class="maat-grid-blog-posts__col__title">Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit.</h2>
-                        <div class="maat-grid-blog-posts__col__info">
-                            <span class="maat-grid-blog-posts__col__info__date">
-                                <i class="fa-solid fa-calendar-days"></i>
-                                <p>23 agosto, 2022</p>
-                            </span>
-                            <span class="maat-grid-blog-posts__col__info__author">
-                                <i class="fa-regular fa-user"></i>
-                                <p>Por Erick Jiménez</p>
-                            </span>
-                        </div>
-                        <hr class="maat-grid-blog-posts__col__divider">
-                        <p class="maat-grid-blog-posts__col__desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et dapibus habitant elementum, faucibus luctus quam aliquet justo. Arcu nascetur non in gravida eu porttitor. Tincidunt sed sed sapien integer. Et pulvinar nunc ullamcorper sagittis,</p>
-                        <a href="#" class="maat-grid-blog-posts__col__link">
-                            LEER MÁS
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <?php
-            while ( have_posts() ) {
-                the_post();
-                $post_link = get_permalink();
-                ?>
-                <article class="post">
-                    <?php
-                    printf( '<h2 class="%s"><a href="%s">%s</a></h2>', 'entry-title', esc_url( $post_link ), wp_kses_post( get_the_title() ) );
-                    printf( '<a href="%s">%s</a>', esc_url( $post_link ), get_the_post_thumbnail( $post, 'large' ) );
-                    the_excerpt();
                     ?>
-                </article>
-            <?php } ?>
+                    <article class="maat-grid-blog-posts__row">
+                        <div class="maat-grid-blog-posts__col">
+                            <?php 
+                                if(!get_the_post_thumbnail()){
+                                    echo "<img style='border: 1px solid #ccc;' src='".get_stylesheet_directory_uri()."/assets/img/photo-not-available.jpg'>";
+                                }
+                            
+                                the_post_thumbnail('full', array('class' => 'maat-grid-blog-posts__col__img'))
+                            ?>
+                        </div>
+                        <div class="maat-grid-blog-posts__col">
+
+                            <?php 
+                             if(count($post_cats) > 0){
+                                $label_cl = "";
+                                foreach ($post_cats as $cat) {
+                                    if($cat->slug == 'gestion-de-residuos'){
+                                        $label_cl = "etiqueta-verde";
+                                    }
+
+                                    if($cat->slug == 'ingenieria-ambiental'){
+                                        $label_cl = "etiqueta-azul";
+                                    }
+
+                                    if($cat->slug == 'recuperacion-y-contingencias'){
+                                        $label_cl = "etiqueta-roja";
+                                    }
+
+                                    if($cat->slug == 'deconstruccion-sostenible'){
+                                        $label_cl = "etiqueta-gris";
+                                    }
+
+                                    ?>
+                                    <a class="maat-link-cat" href="<?php echo get_category_link($cat->cat_ID);?>">
+                                        <span class="maat-grid-blog-posts__col__etiqueta <?php echo $label_cl ?>"><?php echo $cat->name ?></span>
+                                    </a>
+                                    
+                                    <?php
+                                }
+                             }
+                            ?>
+                            
+                            <h2 class="maat-grid-blog-posts__col__title"><?php the_title(); ?></h2>
+                            <div class="maat-grid-blog-posts__col__info">
+                                <span class="maat-grid-blog-posts__col__info__date">
+                                    <i class="fa-solid fa-calendar-days"></i>
+                                    <p><?php echo $post_date; ?></p>
+                                </span>
+                                <span class="maat-grid-blog-posts__col__info__author">
+                                    <i class="fa-regular fa-user"></i>
+                                    <p>Por: <?php echo $post_author_full ?></p>
+                                </span>
+                            </div>
+                            <hr class="maat-grid-blog-posts__col__divider">
+                            <div class="maat-grid-blog-posts__col__desc">
+                               <?php the_excerpt(); ?>
+                            </div>
+                            <a href="<?php the_permalink(); ?>" class="maat-grid-blog-posts__col__link">
+                                LEER MÁS
+                            </a>
+                        </div>
+                    </article>
+                <?php } ?>
+            </div>
+           
         </div>
 
         <?php wp_link_pages(); ?>
@@ -149,7 +136,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                 <div class="nav-next"><?php previous_posts_link( sprintf( __( 'newer %s', 'hello-elementor' ), '<span class="meta-nav">&rarr;</span>' ) ); ?></div>
             </nav>
         <?php endif; ?>
-
+        
     </div>
 	
 </main>
